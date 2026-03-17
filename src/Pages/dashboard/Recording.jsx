6c2559f671c5c4f9bd2475fabea144c373195d30
abdulmuiz <abdulmuizalaya@gmail.com>
2026-03-17 16:14:16 +0100

    hhhhh

---
+++ src/Pages/dashboard/Recording.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./Recorder.css";
+
+function Recorder() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const startRecording = () => {
+    setIsRecording(true);
+    setIsPaused(false);
+  };
+
+  const pauseRecording = () => {
+    setIsPaused(!isPaused);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+    setIsPaused(false);
+    alert("Recording Saved!");
+  };
+
+  return (
+    <div className="container">
+      {!isRecording && (
+        <>
+          <p className="status">• READY TO RECORD</p>
+
+          <div className="mic-button" onClick={startRecording}>
+            🎤
+          </div>
+
+          <p className="instruction">Press to Start Recording</p>
+        </>
+      )}
+
+      {isRecording && (
+        <>
+          <p className="recording">● RECORDING</p>
+
+          <div className="mic-button">🎤</div>
+
+          <p>Tap pause to pause - Stop to save</p>
+
+          <div className="wave-box">
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+          </div>
+
+          <div className="buttons">
+            <button onClick={pauseRecording}>
+              {isPaused ? "Resume" : "Pause"}
+            </button>
+
+            <button className="stop" onClick={stopRecording}>
+              Stop & Save
+            </button>
+          </div>
+        </>
+      )}
+
+      <p className="note">
+        Memo will automatically transcribe your recording and generate a
+        summary when you stop.
+      </p>
+    </div>
+  );
+}
+
+export default Recorder;
